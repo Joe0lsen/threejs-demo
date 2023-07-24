b2a5ec0552c56e0c4fb0fd6b91c2be203a41d589
Joe0lsen <olsen.jcm@gmail.com>
2023-07-24 17:37:01 -0600

    Updates

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const size = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 500);
-camera.position.set(0,1,2);
+camera.position.set(0,3,30);
+camera.rotation.x = -15 * Math.PI / 180;
 
 // Scene
 const scene = new THREE.Scene();
@@ -23,16 +24,39 @@ renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
 
+// Lights
+const light = new THREE.PointLight('#fff',0.8,100);
+light.position.set(10,10,10);
+scene.add(light);
+
+const lightHelper = new THREE.PointLightHelper( light, 1 );
+scene.add( lightHelper );
+
+const ambientLight = new THREE.AmbientLight('#fff',0.05,100);
+ambientLight.position.set(-3,-10,3);
+scene.add(ambientLight);
+
+const backLight = new THREE.PointLight('#fff',0.1,100);
+backLight.position.set(-5,-5,-10);
+scene.add(backLight);
+
+const backLightHelper = new THREE.PointLightHelper( backLight, 1 );
+scene.add( backLightHelper );
+
+
+// Geometry
+const geometry = new THREE.IcosahedronGeometry(3);
+const material = new THREE.MeshStandardMaterial({color:'#0ff', roughness: 0.4, metalness:0.4});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+
+// Animation
 function animate() {
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
-
 animate();
-
-// Geometry
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color:'#0f0'});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
 
